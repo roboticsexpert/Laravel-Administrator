2189b5b03965d09fad667d3e657ead61ec19f397
Mahdi Youseftabar <roboticsexpert@gmail.com>
2016-08-26 15:27:56 +0430

    commit

---
+++ src/lang/fa/administrator.php
@@ -0,0 +1,57 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Administrator Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| en - English
+	|
+	*/
+
+	'dashboard' => 'داشبورد',
+	'edit' => 'ویرایش',
+	'filters' => 'فیلترها',
+	'loading' => '‬بارگذاری...',
+	'createnew' => 'جدید ایجاد کردن',
+	'new' => 'جدید',
+	'viewitem' => 'نمایش :single',
+	'id' => 'ID',
+	'uploadimage' => 'آپلود عکس',
+	'imageuploading' => 'درحال آپلود عکس',
+	'uploadfile' => 'آپلود فایل',
+	'fileuploading' => 'درحال آپلود فایل',
+	'no_image_uploaded' => 'هیچ عکسی آپلود نشده',
+	'no_file_uploaded' => 'هیچ فایلی آپلود نشده',
+	'none' => 'هیچ',
+	'all' => 'همه',
+	'itemsperpage' => 'آیتم در هر صفحه',
+	'noresults' => 'موردی یافت نشد',
+	'backtosite' => 'بازگشت به سایت',
+	'logout' => 'خروج',
+
+	'previous' => 'قبلی',
+	'next' => 'بعدی',
+
+	'close' => 'بستن',
+	'delete' => 'حذف کردن',
+	'save' => 'ذخیره کردن',
+	'create' => 'ایجاد کردن',
+	'cancel' => 'لغو کردن',
+
+	'active' => 'فقط چند لحظه صبر ...',
+	'success' => 'موفقیت آمیز!',
+	'error' => 'مشکلی وجود داشت!',
+
+	'valid_columns' => "You must provide a valid 'columns' array in each model's config",
+	'valid_title' => "You must provide a valid title and single name in each model's config",
+	'valid_model' => "You must provide a 'model' option in each model's config",
+	'valid_edit' => "You must provide a valid 'edit_fields' array in each model's config",
+	'valid_menu' => "You must provide a valid 'menu' option in the administrator.php config",
+	'valid_config_path' => "You must provide a valid 'model_config_path' in the administrator.php config. The directory must also exist and be readable.",
+	'not_eloquent' => " is not an Eloquent model",
+	'storage_path_permissions' => "You must make your storage path writable in order to make a settings page",
+	'valid_home_page' => "You must provide a valid menu item in the home_page option of your config",
+);
